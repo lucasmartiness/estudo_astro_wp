a360863292216a3163f30b8862b5fb0d93251165
lucasmartines <39931785+lucasmartines@users.noreply.github.com>
2023-11-14 10:14:06 -0300

    adicionado modo ssr
    então cruze os dedos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
     image: {
         remotePatterns: [{ protocol: "https" }],
       },
-      compressHTML: false
-
+      compressHTML: false,
+      output: 'server',
+      adapter: netlify(),
 });
+
+ 
